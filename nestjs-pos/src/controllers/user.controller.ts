5bc4f94ec3929d10465a14b8fb6b99ade3429d30
Mastered1738 <vinkomilotic632@gmail.com>
2024-01-26 20:47:26 +0100

    feat: add create user, get all users

--- nestjs-pos/src/controllers/user.controller.ts
+++ nestjs-pos/src/controllers/user.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseInterceptors } from '@nestjs/common';
 import { UserService } from '../providers/user.service';
 import { User } from 'src/entities/user.entity';
 import { CacheInterceptor, CacheKey, CacheTTL } from '@nestjs/cache-manager';
+import { UserDTO } from 'src/dto/user.dto';
 
 @Controller('/user')
 export class UserController {
@@ -9,9 +10,14 @@ export class UserController {
 
   @UseInterceptors(CacheInterceptor)
   @CacheKey('all-users')
-  @CacheTTL(5000)
+  @CacheTTL(60000)
   @Get('/allUsers')
   async getAllUsers(): Promise<User[]> {
     return this.userService.getAllUsers();
+  }
+
+  @Post('/create-user')
+  async createUser(@Body() user: UserDTO): Promise<User> {
+    return this.userService.createUser(user);
   }
 }
--- nestjs-pos/src/providers/user.service.ts
+++ nestjs-pos/src/providers/user.service.ts
@@ -1,6 +1,7 @@
 import { CACHE_MANAGER, CacheStore } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserDTO } from 'src/dto/user.dto';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
 
@@ -11,10 +12,20 @@ export class UserService {
     @Inject(CACHE_MANAGER) private cache: CacheStore,
   ) {}
 
-  async createUser(user: User): Promise<User> {
-    const newUser = await this.userRepo.create(user);
+  async createUser(user: UserDTO): Promise<User> {
+    const newUser = this.userRepo.create({
+      username: user.username,
+      password: user.password,
+      user_type: {
+        user_type_id: user.user_type,
+      },
+    });
 
-    this.cache.set(newUser.username, newUser);
+    await this.userRepo.save(newUser);
+
+    const allUsers = await this.getAllUsers();
+
+    this.cache.set('all-users', allUsers);
 
     return newUser;
   }
